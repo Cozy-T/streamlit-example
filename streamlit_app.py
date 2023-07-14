03673f6fadb2716c7e0518c9621f9618aa1e28ce
Cozy-T <54201186+Cozy-T@users.noreply.github.com>
2023-07-14 21:26:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,31 @@
 from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Medizin-Tagebuch
 """
 
-
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    st.header("Dosis Information")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    dose = st.number_input("Dosis", min_value=1, max_value=4, value=1, step=1)
+    time = st.time_input("Uhrzeit")
+    quantity = st.number_input("Anzahl Menge", min_value=1, value=1, step=1)
+    milligram = st.number_input("Milligram", min_value=1, value=1, step=1)
 
-    points_per_turn = total_points / num_turns
+    st.header("Symptom Information")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    symptom_rating = st.selectbox("Bewertung", ["Negativ", "Leicht Negativ", "Neutral", "Leicht Positiv", "Positiv"])
+    symptom_time = st.time_input("Symptom Zeitstempel")
+    symptom_description = st.text_input("Symptombeschreibung")
+    mood = st.text_input("Stimmung")
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    if st.button("Einreichen"):
+        st.write("Dosis: ", dose)
+        st.write("Uhrzeit: ", time)
+        st.write("Anzahl Menge: ", quantity)
+        st.write("Milligram: ", milligram)
+        st.write("Bewertung: ", symptom_rating)
+        st.write("Symptom Zeitstempel: ", symptom_time)
+        st.write("Symptombeschreibung: ", symptom_description)
+        st.write("Stimmung: ", mood)
